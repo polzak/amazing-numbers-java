5cf874571263e9a3c76bd3281f5639f4e4a24f6a
unknown <hello.polzak@gmail.com>
2021-10-24 00:11:22 +0900

    Step 3: Palindromic Numbers completed.

--- src/numbers/Main.java
+++ src/numbers/Main.java
@@ -5,25 +5,42 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        boolean natural = false;
         boolean divisibleBy7 = false;
         boolean endsWith7 = false;
         boolean even = false;
         boolean buzz = false;
         boolean duck = false;
+        boolean palindromic = false;
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a natural number:");
-        int n = scanner.nextInt();
+        System.out.println("Welcome to Amazing Numbers!\n");
+        System.out.println("Supported requests:");
+        System.out.println("- enter a natural number to know its properties;");
+        System.out.println("- enter 0 to exit.\n");
 
-        if (n <= 0) {
-            System.out.println("This number is not natural!");
-        } else {
-            natural = true;
-        }
+        while (true) {
 
-        if (natural) {
+            divisibleBy7 = false;
+            endsWith7 = false;
+            even = false;
+            buzz = false;
+            duck = false;
+            palindromic = false;
+
+            System.out.print("Enter a request: ");
+            long n = scanner.nextLong();
+            System.out.println();
+
+            // Only a natural number passes through it.
+            if (n < 0) {
+                System.out.println("The first parameter should be a natural number or zero.\n");
+                continue;
+            } else if (n == 0) {
+                System.out.println("Goodbye!");
+                break;
+            }
+
             // decide if it is even or odd
             if (n % 2 == 0) {
                 even = true;
@@ -33,7 +50,7 @@ public class Main {
             if (n % 7 == 0) {
                 divisibleBy7 = true;
             }
-            int d = n % 10;
+            double d = n % 10;
             if (d == 7) {
                 endsWith7 = true;
             }
@@ -41,9 +58,20 @@ public class Main {
                 buzz = true;
             }
 
+            // decide if it is a duck number.
             String stringNumber = "" + n;
             if (stringNumber.contains("0")) {
                 duck = true;
+            }
+
+            //decide if it is a palindromic number.
+            String reversedNumber = "";
+            char[] charNumber = stringNumber.toCharArray();
+            for (char c : charNumber) {
+                reversedNumber = c + reversedNumber;
+            }
+            if (stringNumber.equals(reversedNumber)) {
+                palindromic = true;
             }
 
             System.out.println("Properties of " + n);
@@ -51,6 +79,7 @@ public class Main {
             System.out.println("odd: " + !even);
             System.out.println("buzz: " + buzz);
             System.out.println("duck: " + duck);
+            System.out.println("palindromic: " + palindromic);
         }
     }
 }
